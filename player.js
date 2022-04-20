bc2451f530b32b99454d119af7f4194adb823190
Cyanne Jones <jonescyanne@yahoo.com>
2022-04-20 14:15:41 -0600

    add playerSpacesOccupied to player.js

--- player.js
+++ player.js
@@ -2,7 +2,17 @@ class Player {
   constructor(name, isFirst) {
     this.id = name;
     this.wins = 0;
-    this.SpacesOccupied = [];
+    this.playerSpacesOccupied = {
+      gameBox1: false,
+      gameBox2: false,
+      gameBox3: false,
+      gameBox4: false,
+      gameBox5: false,
+      gameBox6: false,
+      gameBox7: false,
+      gameBox8: false,
+      gameBox9: false,
+    };
     this.isFirst = isFirst;
     this.currentTurn = true;
     if (this.id === 'nacho') {
